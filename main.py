263d843fe190acd946a5d40838a46abbd9a4045c
Space <ashrayvarughese@gmail.com>
2025-08-16 15:28:23 +0530

    i will later work on this

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 
 pygame.init()
-# * main things that screen and caption of the window
+# * main things that screen and caption of the window and the tick speed
+# clock for controlling the frame rate with delta time
+clock = pygame.time.Clock()
+delta_time = 0.1
 
+# configs of the window
 pygame.display.set_caption("flappy bird")
 width_screen = 800
 height_screen = 600
@@ -10,16 +14,19 @@ screen_tuple = (width_screen, height_screen)
 screen = pygame.display.set_mode(screen_tuple)
 
 # * down this is the player things
-player_sprite = pygame.image.load('bird.png').convert_alpha() # Load the player image
 
+player_gravity = 10  # Gravity effect on the player
+player_jump_height = 10  # Height of the player's jump
+
+# player sprite loading and scaling
+player_sprite = pygame.image.load('bird.png').convert_alpha() # Load the player image
 player_hight = 50
 player_width = 50
 player_sprite = pygame.transform.scale(player_sprite, (player_width, player_hight))  # Scale the player image
 
 # player positions
-player_x = 100
+player_x = 200
 player_y = 50
-player_tuple = (player_x, player_y)
 
 # * main loop
 running = True
@@ -30,8 +37,15 @@ while running:
     screen.fill((0, 0, 0))  # background color
     # * above this is the main things i would not change other than the background
     
-    screen.blit(player_sprite, player_tuple)
+    player_y = player_y + player_gravity
+    print(player_y)
     
+    
+    screen.blit(player_sprite, (player_x, player_y))  # Draw the player sprite at the position in the tuple
+    
+    # * this is the update line and the fps line no need to tinker with this that much
     pygame.display.flip()  # Update the display
+    
+    clock.tick(60)  # Set the frame rate to 60 FPS
+
 pygame.quit()
-exit()
